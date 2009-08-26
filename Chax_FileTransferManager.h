3f2ce018f97f391e5d25efbf5d432e6676c02050
Kent Sutherland <kent.sutherland@gmail.com>
2009-08-26 00:09:01 -0400

    Reimplemented remove completed file transfers.

---
+++ Chax_FileTransferManager.h
@@ -0,0 +1,15 @@
+//
+//  Chax_FileTransferManager.h
+//  Chax
+//
+//  Created by Kent Sutherland on 8/25/09.
+//  Copyright 2009 Kent Sutherland. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Chax_FileTransferManager : NSObject {
+
+}
+
+@end
---
+++ Chax_FileTransferManager.m
@@ -0,0 +1,71 @@
+//
+//  Chax_FileTransferManager.m
+//  Chax
+//
+//  Created by Kent Sutherland on 8/25/09.
+//  Copyright 2009 Kent Sutherland. All rights reserved.
+//
+
+#import "Chax_FileTransferManager.h"
+#import "IMFoundation.h"
+#import "IMCore.h"
+#import "iChat5.h"
+
+@implementation Chax_FileTransferManager
+
++ (void)load
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(chax_fileTransferUpdatedNotification:) name:@"IMFileTransferUpdatedNotification" object:nil];
+}
+
++ (void)chax_fileTransferUpdatedNotification:(NSNotification *)note
+{
+    if ([Chax boolForKey:@"AutoclearFileTransfers"]) {
+        IMFileTransfer *transfer = [note object];
+        
+        if ([transfer transferState] == 4 && [transfer error] == 0) {
+            [self cancelPreviousPerformRequestsWithTarget:self selector:@selector(chax_removeSuccessfulTransfers) object:nil];
+            [self performSelector:@selector(chax_removeSuccessfulTransfers) withObject:nil afterDelay:2.0];
+        }
+    }
+}
+
++ (void)chax_removeSuccessfulTransfers
+{
+    FileTransferCenter *transferCenter = [NSClassFromString(@"FileTransferCenter") sharedInstance];
+    FileTransferManager *transferManager = [NSClassFromString(@"FileTransferManager") sharedInstance];
+    
+    NSSet *transfers = [transferManager _transfersMatchingCondition:1 wantsGUIDs:NO];
+    ViewTable *viewTable = [transferManager valueForKey:@"_table"];
+    NSArray *subviews = [viewTable subviews];
+    
+    for (NSView *nextView in subviews) {
+        if ([nextView isKindOfClass:NSClassFromString(@"FileTransferView")]) {
+            [(FileTransferView *)nextView _deselect];
+        }
+    }
+    
+    for (NSView *nextView in subviews) {
+        if ([nextView isKindOfClass:NSClassFromString(@"FileTransferView")]) {
+            FileTransferView *fileTransferView = (FileTransferView *)nextView;
+            FileTransfer *fileTransfer = [fileTransferView fileTransfer];
+            
+            if ([fileTransfer error] == 0 && [transfers containsObject:fileTransfer]) {
+                [fileTransferView _setSelected];
+            }
+        }
+    }
+    
+    [viewTable selectionDidChange];
+    
+    [transferManager deleteSelectedRowsInViewTable:nil];
+}
+
+- (void)chax_swizzle_windowDidLoad
+{
+    [self chax_swizzle_windowDidLoad];
+    
+    [[self class] chax_removeSuccessfulTransfers];
+}
+
+@end
